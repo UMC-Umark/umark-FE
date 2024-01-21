2d38c4a341a214dae59790dfee091bc9a45dab36
rony <ahr020532@gmail.com>
2024-01-21 23:15:51 +0900

    modified : Intro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Intro from "./pages/Intro";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path ="/Intro" element ={<Intro/>}/>
         </Routes>
       </BrowserRouter>
     </div>
