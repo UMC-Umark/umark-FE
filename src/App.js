3517e83bed011ca99411013ddbf2702a6909bdcb
honi <winner020531@naver.com>
2024-01-21 23:14:53 +0900

    [FEAT}-write bookmark

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Bookmark from './pages/Bookmark';
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/write" element={<Bookmark />} />
         </Routes>
       </BrowserRouter>
     </div>
