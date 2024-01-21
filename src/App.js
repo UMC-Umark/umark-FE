671fbd83b609140dbacec892cdbe9895359a23ad
sehyeo <icomksh@gmail.com>
2024-01-22 07:13:13 +0900

    signup page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Intro from "./pages/Intro";
-<<<<<<< HEAD
 import Bookmark from "./pages/Bookmark";
 import Signup from "./pages/Signup";
-=======
-import Bookmark from './pages/Bookmark';
 import Report from "./pages/Reporting";
 import Recommend from "./pages/Recommend";
->>>>>>> fedc89479c4ce529657315f03b702dc85c818ace
 
 function App() {
   return (
@@ -18,12 +14,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/Intro" element={<Intro />} />
           <Route path="/write" element={<Bookmark />} />
-<<<<<<< HEAD
           <Route path="/Signup" element={<Signup />} />
-=======
-          <Route path="/Reporting" element={<Report/>}/>
-          <Route path="/Recommend" element={<Recommend/>}/>
->>>>>>> fedc89479c4ce529657315f03b702dc85c818ace
+          <Route path="/Reporting" element={<Report />} />
+          <Route path="/Recommend" element={<Recommend />} />
         </Routes>
       </BrowserRouter>
     </div>
