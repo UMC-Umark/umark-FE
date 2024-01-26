563f5648a42b5454ccd99746ca54446fd4d82f42
honi <winner020531@naver.com>
2024-01-26 23:45:12 +0900

    [FEAT]-Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ import arrow from "../img/arrow.png";
 
 export default function Login() {
   return (
-    <div className="h-screen bg-black text-white">
+    <div className="h-full bg-black text-white">
       <br />
       <div className="w-full flex justify-between items-center">
         <Link className="custom-arrow" to="/">
