335eac3aab5ed4578a101713360ea1ddd699e5c4
rony <ahr020532@gmail.com>
2024-02-06 11:42:17 +0900

    [FEAT] report

--- src/pages/Reporting.jsx
+++ src/pages/Reporting.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import back from '../img/back_icon.png';
 
-// 신고 옵션을 선택하는 라디오 버튼 컴포넌트
 const RadioOptions = ({ options, selectedOption, setSelectedOption, onOtherSelected }) => {
   return (
     <>
@@ -33,28 +33,32 @@ const ReportingPage = () => {
   const [isOtherSelected, setIsOtherSelected] = useState(false);
   const [otherText, setOtherText] = useState('');
   const [showModal, setShowModal] = useState(false); // 모달 표시 상태
+  
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+  
+    // report 값 설정
+    const reportValue = reportOptions.indexOf(selectedOption) + 1; // 옵션에 따라 1부터 시작하는 인덱스 값을 사용
+  
     const reportData = {
-      reason: selectedOption,
-      additionalInfo: isOtherSelected ? otherText : ''
+      memberId: 1, // 예시로 1을 사용, 실제 memberId 값으로 변경해야 함
+      bookMarkId: 1, // 예시로 1을 사용, 실제 bookMarkId 값으로 변경해야 함
+      report: reportValue, // 선택된 옵션의 인덱스 + 1
+      reason: isOtherSelected ? otherText : selectedOption // 기타를 선택한 경우 otherText, 아니면 선택된 옵션 사용
     };
-
+  
     // API 호출 및 처리
     try {
-      // 백엔드로 데이터 전송
-      const response = await axios.post('http://localhost:3000/report', reportData);
-      
+      const response = await axios.post('http://15.165.194.140/report', reportData);
       console.log('신고가 정상적으로 처리되었습니다.', response.data);
       setShowModal(true);
-
     } catch (error) {
       console.error('신고 제출 중 오류가 발생했습니다:', error);
       // 에러 처리 로직 추가 (예: 상태 업데이트, 사용자에게 메시지 표시 등)
     }
   };
+  
 
   const handleCloseModal = () => {
     setShowModal(false);
@@ -71,17 +75,20 @@ const ReportingPage = () => {
   return (
     <div className="flex flex-col h-screen bg-white text-black">
       <nav className="flex items-center justify-between p-6 border-b border-gray-200 h-20">
-        <button onClick={() => navigate(-1)} className="text-black">
-          <span className="material-icons">back</span>
-        </button>
+      <button onClick={() => navigate(-1)} className="text-black">
+  <img src={back} alt="아이콘" className="mx-auto my-3 h-8 back-icon" />
+</button>
+
         <h1 className="text-center font-bold text-lg">신고하기</h1>
-        <div style={{ width: '24px' }}></div> 
+        <div style={{ width: "24px" }}></div>
       </nav>
 
       <div className="flex-grow flex flex-col items-center px-4 py-2">
         <form onSubmit={handleSubmit} className="w-full max-w-md text-center">
           <fieldset className="mb-4">
-            <legend className="text-lg mt-4 mb-8 font-bold">해당 게시물을 신고하는 이유를 알려주세요</legend>
+            <legend className="text-lg mt-4 mb-8 font-bold">
+              해당 게시물을 신고하는 이유를 알려주세요
+            </legend>
             <RadioOptions
               options={reportOptions}
               selectedOption={selectedOption}
@@ -98,26 +105,28 @@ const ReportingPage = () => {
               />
             )}
           </fieldset>
-          <button type="submit" className="mt-4 w-full bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded">
+          <button
+            type="submit"
+            className="mt-4 w-full bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded"
+          >
             제출하기
           </button>
         </form>
       </div>
 
       {showModal && (
-  <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center">
-    <div className="text-white bg-transparent p-4 rounded-lg text-center">
-      <p className="text-lg pb-4">신고가 정상적으로 제출되었어요.</p>
-      <button 
-          onClick={handleCloseModal} 
-          className="bg-white text-black font-bold py-2 px-4 rounded focus:outline-none transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg"
-        >
-        확인
-      </button>
-    </div>
-  </div>
-)}
-
+        <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center">
+          <div className="text-white bg-transparent p-4 rounded-lg text-center">
+            <p className="text-lg pb-4">신고가 정상적으로 제출되었어요.</p>
+            <button
+              onClick={handleCloseModal}
+              className="bg-white text-black font-bold py-2 px-4 rounded focus:outline-none transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg"
+            >
+              확인
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
