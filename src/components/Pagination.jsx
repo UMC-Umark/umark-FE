ebe58a7c496358bad115ddcd60481b075127eb2d
qwertybbk <qwerty_bbk@naver.com>
2024-02-17 22:58:16 +0900

    Pagination 적용

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,35 +1,50 @@
 import React from 'react';
-import leftArrowIcon from '../img/arrow-left.png';
-import rightArrowIcon from '../img/arrow-right.png';
+//import leftArrowIcon from '../img/arrow-left.png';
+//import rightArrowIcon from '../img/arrow-right.png';
 import './Pagination.css';
 
-function Pagination({ totalPages, currentPage, onPageChange }) {
-  const pages = [...Array(totalPages).keys()].map(num => num + 1);
+function Pagination ({ pageNumber, totalPages, limit, setPageNumber }){
+    const numPages = Math.ceil(totalPages/limit);
+    const firstNum = pageNumber - (pageNumber % 5) + 1;
+    const lastNum = pageNumber - (pageNumber % 5) + 5;
 
-  return (
-    <div className="pagination">
-      <button 
-        className={currentPage === 1 ? 'disabled' : ''}
-        onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 1}>
-        <img src={leftArrowIcon} alt="Previous" />
-      </button>
-      {pages.map(page => (
-        <button 
-          key={page}
-          className={currentPage === page ? 'active' : ''}
-          onClick={() => onPageChange(page)}>
-          {page}
-        </button>
-      ))}
-      <button 
-        className={currentPage === totalPages ? 'disabled' : ''}
-        onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}>
-        <img src={rightArrowIcon} alt="Next" />
-      </button>
-    </div>
-  );
+    const renderButtons = () => {
+      const buttons = [];
+      for (let i = firstNum; i <= lastNum; i++) {
+        if (i <= totalPages) {
+          buttons.push(
+            <button className='page-button' 
+              key={i} 
+              onClick={() => setPageNumber(i)}
+              aria-current={pageNumber === i ? "pageNumber" : null}
+            >
+              {i}
+            </button>
+          );
+        }
+      }
+      return buttons;
+    };
+
+    return (
+        <div className='pagination flex justify-center items-center gap-1 m-4'>
+            <div className='button-wrap'>
+                <button className='arrow-button'
+                    onClick={() => {setPageNumber(pageNumber-1);}} 
+                    disabled={pageNumber === 1}
+                >
+                  {"<"} 
+                </button>
+                {renderButtons()}
+                <button className='arrow-button'
+                    onClick={() => {setPageNumber(pageNumber+1);}} 
+                    disabled={pageNumber === numPages}
+                >
+                  {">"}
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Pagination;
