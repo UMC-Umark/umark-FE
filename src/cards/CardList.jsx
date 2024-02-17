f805c11aa39038fd3a29a78ac15b929ccbbe38e7
qwertybbk <qwerty_bbk@naver.com>
2024-02-17 17:17:20 +0900

    resolve pull request conflict

--- src/cards/CardList.jsx
+++ src/cards/CardList.jsx
@@ -5,12 +5,13 @@ export default function CardList({ cardsData, onClick, myLike }) {
     return (
         <div className='row'>
             {cardsData.map((bookmark) => {
+                // console.log(bookmark); 
                 return (
                     <div key={bookmark.id} className="col-lg-4 col-md-6 col-sm-auto mt-5">
                         <Card
                             {...bookmark}
                             onClick={onClick}
-                            isReported={bookmark.isReported} // isReported prop 전달
+                            isReported={bookmark.reported} // isReported prop 전달
                             myLike={myLike}
                         />
                     </div>
