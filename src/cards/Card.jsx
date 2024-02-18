a24914bf535f96d7db0fc3526fb2803d29d6de33
qwertybbk <qwerty_bbk@naver.com>
2024-02-19 02:50:43 +0900

    adjust isReported Card .css

--- src/cards/Card.jsx
+++ src/cards/Card.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { reportedData } from './CardReported'; // CardReported.jsx 파일에서 reportedData를 import
 
 import bookMark from '../img/bookMark.png';
 import bookMarkFill from '../img/bookMarkFill.png';
@@ -13,6 +14,9 @@ export default function Card({ id, title, createdAt, hashTagContent, content, ur
 
     const date = new Date(createdAt);
     const formattedTime = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')} ${date.getHours()}:${date.getMinutes()}`;
+
+    // reportedData 배열에서 데이터를 가져와서 변수에 할당
+    const { reportedTitle, reportedContent } = reportedData[0];
 
     const handleLike = async () => {
         if (!accessToken) {
@@ -47,7 +51,7 @@ export default function Card({ id, title, createdAt, hashTagContent, content, ur
         <div className="card">
             <div className="card-header">
                 <div className="header-wrap">
-                    <h4>{isReported ? "부적절한 게시물입니다." : title}</h4> 
+                    <h4>{isReported ? reportedTitle : title}</h4> 
                     {isReported ? null : (
                         <div>
                             <img 
@@ -77,7 +81,7 @@ export default function Card({ id, title, createdAt, hashTagContent, content, ur
                 </div>
             </div>
             <div className="card-body">
-                <p className="text-para">{isReported ? "신고 누적으로 블라인드 처리된 게시물입니다." : content}</p>
+                <p className="text-para">{isReported ? reportedContent : content}</p>
             </div>
             <div className="card-footer">
                 {isReported ? (
---
+++ src/cards/CardReported.jsx
@@ -0,0 +1,6 @@
+export const reportedData = [
+    {
+        reportedTitle: '부적절한 게시물입니다.',
+        reportedContent: '신고 누적으로 블라인드 처리된 게시물입니다.',
+    },
+];
